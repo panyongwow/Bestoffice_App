60d88c5996d2e3de9175284a049408851cc75d69
panyongwow <panyongwow@163.com>
2018-12-05 08:57:45 +0800

    2018-12-05上班

--- test/navigation/appNavigation.js
+++ test/navigation/appNavigation.js
@@ -1,5 +1,6 @@
 import {createStackNavigator,createDrawerNavigator,createBottomTabNavigator,createTabNavigator,StackNavigator} from 'react-navigation'
 import React from 'react'
+import {View,Button,Text} from 'react-native'
 import HomePage from './HomePage'
 import Details from './Page1'
 import Setting from './Page2'
@@ -39,14 +40,59 @@ import Setting from './Page2'
 //     Setting:Setting
 // })
 
+class InfoPage extends React.Component{
+    static navigationOptions={
+        drawerLabel:'Info'
+    };
+    constructor(){
+       super()
+    }
+    componentDidMount(){
+        this.props.navigation.openDrawer()
+    }
+    render(){
+        return(
+            <View>
+                <Text>这是我的信息</Text>
+                <Button 
+                    title='打开' 
+                    onPress={()=>{
+                        this.props.navigation.openDrawer()
+                    }}
+                />
+            </View>
+        )
+    }
+}
+
+class PointPage extends React.Component{
+    static navigationOptions={
+        drawerLabel:'Point'
+    };
+    render(){
+        return(
+            <View>
+                <Text>这是我的积分</Text>
+            </View>
+        )
+    }
+}
+
+const MyStack=createDrawerNavigator({
+    Info:InfoPage,
+    Point:PointPage
+})
+
 export default createBottomTabNavigator({
     Home:HomePage,
-    Details:Details
+    Details:Details,
+    My:MyStack
 },{
     navigationOptions:({navigation})=>({
         header:null,
     })
 });
+
 
 // export default class App extends React.Component {
 //     render() {

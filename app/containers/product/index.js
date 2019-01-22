a9ce8a335f20fe7ae004c4682c2f1dd94065b6b8
panyongwow <panyongwow@163.com>
2019-01-22 17:36:03 +0800

    2019-01-22下班

--- app/containers/product/index.js
+++ app/containers/product/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text, TextInput, Image, Button, WebView, StyleSheet, ScrollView, TouchableOpacity, Dimensions, Platform, NativeModules } from 'react-native'
+import AutoHeightWebView from 'react-native-webview-autoheight'
 import Swiper from 'react-native-swiper'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import Entypo from 'react-native-vector-icons/Entypo'
@@ -31,12 +32,10 @@ export default class Product extends Component {
                             showsPagination={true}
                         >
                             <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-                                <Image resizeMode="contain" style={{ height: 300, width: 300 }} source={{ uri: 'http://img.bestoffice.cn/product/103/25682/7902099e947c519208f61e3f9bbc02a5_l.jpg' }} />
+                                {/* <Image resizeMode="contain" style={{ height: 300, width: 300 }} source={{ uri: 'http://img.bestoffice.cn/product/103/25682/7902099e947c519208f61e3f9bbc02a5_l.jpg' }} /> */}
+                                <Image resizeMode="contain" style={{ height: 300, width: 300 }} source={{ uri: 'http://img.bestoffice.cn/product/88/29467/20180629112028_2760_l.jpg' }} />
 
                             </View>
-                            {/* <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-                            <Image resizeMode="contain" style={{ height: 300, width: 300 }} source={{ uri: 'http://img.bestoffice.cn/product/106/22685/3a8eb06ba771f8b0056cb46cca9a3211_l.jpg' }} />
-                        </View> */}
                         </Swiper>
                     </View>
                     <View style={{ paddingLeft: 10, paddingRight: 10 }}>
@@ -44,12 +43,12 @@ export default class Product extends Component {
                             <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                                 <Text style={{ color: 'red', fontSize: 18 }}>
                                     &yen;&nbsp;
-                                    <Text style={{ color: 'red', fontSize: 22, fontWeight: 'bold' }}>6.41</Text>
+                                    <Text style={{ color: 'red', fontSize: 22, fontWeight: 'bold' }}>179</Text>
                                 </Text>
 
-                                <View  style={{flexDirection:'row'}}>
+                                <View style={{ flexDirection: 'row' }}>
                                     <TouchableOpacity
-                                        style={{marginRight:10}}
+                                        style={{ marginRight: 10 }}
                                         onPress={() => { alert('收藏') }}
                                     >
                                         <AntDesign name='hearto' size={18} color='red' />
@@ -60,10 +59,11 @@ export default class Product extends Component {
                                     >
                                         <Entypo name='tools' size={18} color='red' />
                                         <Text style={{ fontSize: 10, color: 'red' }}>售后</Text>
-                                    </TouchableOpacity>                                    
+                                    </TouchableOpacity>
                                 </View>
                             </View>
-                            <Text style={{ fontSize: 15, fontWeight: 'bold' }}>3M 思高 胶带座 进口 H-127</Text>
+                            <Text style={{ fontSize: 15, fontWeight: 'bold' }}>富士施乐（Fuji Xerox） CP105B 标准容量黑色墨粉 CT202545</Text>
+                            <Text style={{ fontSize: 12}}>适用机型 CP105b；CM215fw；CM215b；CM215f；CM205b；CM205f；CP215；CP215w</Text>
                             <View style={{ flexDirection: 'row', marginTop: 5 }}>
                                 <Text style={styles.tag}>特价</Text>
                                 <Text style={styles.tag}>协议价</Text>
@@ -85,8 +85,8 @@ export default class Product extends Component {
                         <View style={[styles.viewborder, { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap' }]}>
                             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                                 <Text style={{ fontSize: 13, color: '#adadad', marginRight: 15 }}>参数</Text>
-                                <Text style={{ fontSize: 12,marginRight:10 }}>编码:1030025682&nbsp;&nbsp;&nbsp;</Text>
-                                <Text style={{ fontSize: 12,marginRight:10 }}>品牌:3M&nbsp;&nbsp;&nbsp;</Text>
+                                <Text style={{ fontSize: 12, marginRight: 10 }}>编码:1030025682&nbsp;&nbsp;&nbsp;</Text>
+                                <Text style={{ fontSize: 12, marginRight: 10 }}>品牌:3M&nbsp;&nbsp;&nbsp;</Text>
                                 <Text style={{ fontSize: 12 }}>单位:个</Text>
                             </View>
                             <Text style={{ textAlignVertical: 'center', fontSize: 12 }}>更多&gt;</Text>
@@ -102,15 +102,27 @@ export default class Product extends Component {
                             </View>
                             <Text style={{ textAlignVertical: 'center', fontSize: 12 }}>更改&gt;</Text>
                         </TouchableOpacity>
+                        <View style={styles.viewborder}>
+                            {/* <View style={{ flexDirection: 'row', alignItems: 'center' }}> */}
+                                <Text style={{ fontSize: 13, color: '#adadad', marginRight: 15 }}>商品介绍</Text>
+                            {/* </View> */}
+                            <AutoHeightWebView
+                                originWhitelist={['*']}
+                                // source={{ html: '<div> </div> <div> </div> <div> </div> <div> </div> <div> </div> <div> </div> <p style="color:#222222;text-align:center;">  <span style="color:#444444;"><strong><span style="font-size:0.18rem">价格适宜,可有效降低您的办公成本;</span></strong></span><span style="color:#444444;"><strong><span style="font-size:0.18rem;"><br /> <strong>采用全自动化裁切工艺，良好的品质管理、质量稳定性好；<br /> 白度符合国际标准环保白度；<br /> 挺度好，不卡纸，经静电处理，运行性好；<br /> 适用于复印机、镭射传真机、喷墨机、印刷机等使用</strong></span></strong></span>  </p>' }}
+                                source={{html:'<div style="font-size:0.8rem"><p>  功能特点：设计新颖、外观精巧、刀锋锐安全 </p></div>'}}
+                            />
+                            <AutoHeightWebView
+                                originWhitelist={['*']}
+                                //source={{html:'<div style="font-size:0.8rem><p><img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/25224/30/2405/306629/5c1c7abcE20119d2c/fb55f6107a23c920.jpg" align="absmiddle" /> </p></div>'}}
 
-
-
-
+                                source={{html:'<div style="font-size:0.8rem><p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/25224/30/2405/306629/5c1c7abcE20119d2c/fb55f6107a23c920.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/24030/18/2483/132267/5c1c7abcE6d90720b/8ca7a2a054c43cdc.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/29095/19/2399/186720/5c1c7abcEef845f14/9e772372b41f6878.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/20851/33/2417/177570/5c1c7abcE5ce1ee3f/2d0a311c05e4c055.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/28725/18/2371/186295/5c1c7abcEf44330f7/7c77f10e245e43ba.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/26621/9/2438/200861/5c1c7abcEf980b06a/2887575669250aaf.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/14789/33/2419/263276/5c1c7abcE090de2f1/3e29ca4fd7171697.jpg" align="absmiddle" /> </p></div>'}}
+                            />    
+                            {/* <AutoHeightWebView
+                                originWhitelist={['*']}
+                                source={{html:'<div style="font-size:0.8rem"><table>  <tbody>   <tr>    <td class="de-feature">     型号    </td>    <td class="de-value">     B22AA3s    </td>    <td class="de-feature">     产品规格    </td>    <td class="de-value">     10卷/提，10提/箱，245段/卷,3层    </td>    <td class="de-feature">     纸巾尺寸（cm）    </td>    <td class="de-value">     11.0 X 10.0    </td>   </tr>   <tr>    <td class="de-feature">     保质期    </td>    <td class="de-value">     3年    </td>    <td class="de-feature">     材质    </td>    <td class="de-value">     100% 原木浆    </td>    <td class="de-feature">     香型    </td>    <td class="de-value">     无香    </td>   </tr>   <tr>    <td class="de-feature">     纸品等级    </td>    <td class="de-value">     合格品    </td>    <td class="de-feature">     加工定制    </td>    <td class="de-value">     是    </td>    <td class="de-feature">     货号    </td>    <td class="de-value">     B22AA3s    </td>   </tr>   <tr>    <td class="de-feature">     层数    </td>    <td class="de-value">     3层    </td>    <td class="de-feature">     是否含香味    </td>    <td class="de-value">     否    </td>    <td class="de-feature">     片数    </td>    <td class="de-value">     10    </td>   </tr>  </tbody> </table> <br /></div>'}}
+                            />                                                     */}
+                        </View>
                     </View>
-                    {/* <WebView style={{ height: 600 }}
-                    originWhitelist={['*']}
-                    source={{ html: '<div> </div> <div> </div> <div> </div> <div> </div> <div> </div> <div> </div> <p style="color:#222222;text-align:center;">  <span style="color:#444444;"><strong><span style="font-size:0.18rem">价格适宜,可有效降低您的办公成本;</span></strong></span><span style="color:#444444;"><strong><span style="font-size:0.18rem;"><br /> <strong>采用全自动化裁切工艺，良好的品质管理、质量稳定性好；<br /> 白度符合国际标准环保白度；<br /> 挺度好，不卡纸，经静电处理，运行性好；<br /> 适用于复印机、镭射传真机、喷墨机、印刷机等使用</strong></span></strong></span>  </p>' }}
-                /> */}
                 </ScrollView>
                 <View style={{ flexDirection: 'row', borderTopColor: '#f3f3f3', borderTopWidth: 1 }}>
                     <TouchableOpacity

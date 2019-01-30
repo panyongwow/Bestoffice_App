e2e0231e116d6aef63f654107db211aca70bd11c
panyongwow <panyongwow@163.com>
2019-01-30 17:32:45 +0800

    2019-01-30下班

--- app/components/productdetail/buynum.js
+++ app/components/productdetail/buynum.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, Modal, Image, TextInput, TouchableHighlight, StyleSheet } from 'react-native'
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 //商品单页-购买数量区域
 export default class BuyNum extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            buyNum: 1,
+            buyNum: 2,
             buyNumModalVisible: false
         }
     }
@@ -19,18 +20,168 @@ export default class BuyNum extends Component {
         this.setState({
             buyNum: num
         })
+        this.props.changeBuyNum(num)   //传递购买数量给父组件
     }
     render() {
         return (
-            <TouchableOpacity style={[this.props.style, { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', height: 50 }]}
-                onPress={this.showBuyNumModal.bind(this)}
+            <View>
+                <BuyNumModal
+                    showModal={this.state.buyNumModalVisible}
+                    buyNum={this.state.buyNum}
+                    data={this.props.data}
+                    changeBuyNum={(num) => {
+                        this.changeBuyNum(num)
+                    }}
+                />
+
+                <TouchableOpacity style={[this.props.style, { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', height: 50 }]}
+                    onPress={() => {
+                        this.showBuyNumModal()
+                    }
+                    }
+                >
+                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                        <Text style={{ fontSize: 13, color: '#adadad', marginRight: 15 }}>已选</Text>
+                        <Text style={{ fontSize: 14, fontWeight: 'bold' }}>{this.state.buyNum}个</Text>
+                    </View>
+                    <Text style={{ textAlignVertical: 'center', fontSize: 12 }}>更改&gt;</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+//购买数量弹出框
+class BuyNumModal extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            modalVisible: false,
+            buyNum: props.buyNum
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.showModal) {
+            this.setState({
+                modalVisible: true
+            })
+        }
+    }
+    componentDidMount() {
+        //  alert(this.props.data.pic_detail[0].picname)
+    }
+    // setModalVisible = (visible) => {
+    //     // if (!visible) {
+    //          this.props.changeBuyNum(this.state.buyNum)
+    //     // }
+    //     this.setState({ modalVisible: visible });
+
+    // }
+    closeModal() {
+
+        setTimeout(() => {
+            this.setState({
+                modalVisible: false
+            })
+        });
+        //this.props.changeBuyNum(this.state.buyNum)
+
+    }
+    render() {
+        //let pic_s = this.props.data.pic_detail[0].picname.replace(/_l.jpg/g, '_s.jpg')
+        let p = this.props.data
+        return (
+            <Modal
+                animationType='fade'
+                transparent={true}
+                visible={this.state.modalVisible}
+                onRequestClose={() => {
+                    console.log('close')
+                }}
             >
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <Text style={{ fontSize: 13, color: '#adadad', marginRight: 15 }}>已选</Text>
-                    <Text style={{ fontSize: 14, fontWeight: 'bold' }}>{this.state.buyNum}个</Text>
+                <View style={{ flex: 1, flexDirection: 'column-reverse', backgroundColor: 'rgba(0, 0, 0, 0.3)' }}>
+                    <View style={{ justifyContent: 'space-between', backgroundColor: '#fff', height: 350 }}>
+                        {/* <Text
+                            onPress={() => {
+                                this.closeModal()
+                            }}
+                            style={{ fontSize: 20, marginTop: 10 }}>
+                            关闭
+                        </Text>
+                        <TextInput
+                            style={{ height: 30, width: 100, borderColor: 'red', borderWidth: 1, padding: 5 }}
+                            value={this.state.buyNum.toString()}
+                            onChangeText={(text) => {
+                                this.setState({
+                                    buyNum: text
+                                })
+                            }}
+                        /> */}
+                        <View style={{ height: 250 }}>
+                            <TouchableOpacity style={{ flexDirection: 'row-reverse' }}
+                                activeOpacity={0.7}
+                                onPress={() => {
+                                    this.closeModal()
+                                }}
+                            >
+                                <AntDesign name='close' size={22} color='gray' style={{ marginTop: 10, marginRight: 10 }} />
+                            </TouchableOpacity>
+                            <View style={{ flexDirection: 'row' }}>
+                                <Image
+                                    style={{ height: 75, width: 75, marginLeft: 10 }}
+                                    source={{ uri: p.pic_detail[0].picname }}
+                                />
+                                <View style={{ flex: 1, justifyContent: 'space-around', height: 75, paddingLeft: 10, paddingRight: 10 }}>
+                                    <Text style={{ fontSize: 14, fontWeight: 'bold' }}>{p.name}</Text>
+                                    <Text style={{ fontSize: 12}}>商品编码:&nbsp;{p.innercode}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;单位:&nbsp;{p.measurement}</Text>
+                                    <Text style={{ color: 'red', fontSize: 14 }}>
+                                        &yen;&nbsp;
+                                        <Text style={{ color: 'red', fontSize: 18, fontWeight: 'bold' }}>{p.price}</Text>
+                                    </Text>
+
+                                </View>
+                            </View>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent:'space-between', margin: 10,marginTop:20,paddingTop:10,paddingBottom:10, borderBottomColor:'#f3f3f3',borderBottomWidth:1,borderTopColor:'#f3f3f3',borderTopWidth:1 }}>
+                                <Text style={{ fontSize: 16 }}>购买数量</Text>
+                                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                                    <TouchableOpacity  style={{backgroundColor:'#f3f3f3',width:36,height:36}}>
+                                        <AntDesign name='minus' size={20} color='gray' style={{lineHeight:36,marginLeft:8}} />
+                                    </TouchableOpacity>
+                                    <TextInput
+                                        style={{ height: 30, width: 50, padding:0,textAlign: 'center',fontSize:20,fontWeight:'bold', lineHeight:30}}
+                                        value={this.state.buyNum.toString()}
+                                        onChangeText={(text) => {
+                                            this.setState({
+                                                buyNum: text
+                                            })
+                                        }}
+                                    />
+                                    <TouchableOpacity  style={{backgroundColor:'#f3f3f3',width:36,height:36}}>
+                                        <AntDesign name='plus' size={20} color='gray' style={{lineHeight:36,marginLeft:8}} />
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                        <TouchableOpacity
+                            activeOpacity={0.7}
+                            onPress={() => {
+                                alert('加入购物车！')
+                            }}
+                        >
+                            <Text style={{ backgroundColor: '#f00', color: '#fff', textAlign: 'center', lineHeight: 50, height: 50, fontSize: 16, fontWeight: 'bold' }}>加入购物车</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <TouchableOpacity
+                        onPress={() => {
+                            this.closeModal()
+                        }}
+                    >
+                        <View style={{ height: 1000 }}></View>
+                    </TouchableOpacity>
                 </View>
-                <Text style={{ textAlignVertical: 'center', fontSize: 12 }}>更改&gt;</Text>
-            </TouchableOpacity>
+            </Modal>
         )
     }
 }
--- app/containers/product/index.js
+++ app/containers/product/index.js
@@ -22,8 +22,7 @@ export default class Product extends Component {
         const StatusBarHeight = Platform.OS === 'ios' ? 20 : StatusBarManager.HEIGHT;
         this.screenHeight = height - StatusBarHeight - 51;
         this.state = {
-            buyNum: 2,
-            buyNumModalVisible: false,
+            buyNum: 1,
             data: []
         }
     }
@@ -34,14 +33,10 @@ export default class Product extends Component {
         //let id=26461
         let id = 27237
         id = 2734
-        //id=29823
+       // id=29823
         this.productGet(id)
     }
-    showBuyNumModal() {
-        this.setState({
-            buyNumModalVisible: true
-        })
-    }
+
     changeBuyNum(num) {
         this.setState({
             buyNum: num
@@ -54,7 +49,7 @@ export default class Product extends Component {
                 this.setState({
                     data: result
                 })
-                //alert(JSON.stringify(result))
+               // alert(JSON.stringify(result))
             })
             .catch(error => {
                 alert(error)
@@ -67,11 +62,6 @@ export default class Product extends Component {
                 {
                     this.state.data.count > 0
                         ? <ScrollView style={{ height: this.screenHeight }}>
-                            <BuyNumModal
-                                showModal={this.state.buyNumModalVisible}
-                                buyNum={this.state.buyNum}
-                                changeBuyNum={(num) => { this.changeBuyNum(num) }}
-                            />
                             <SwiperComponent data={this.state.data} style={styles.viewborder} />
                             <View style={{ paddingLeft: 10, paddingRight: 10 }}>
                                 <NameDesc data={this.state.data} style={styles.viewborder} />
@@ -81,7 +71,11 @@ export default class Product extends Component {
                                         : null
                                 }
                                 <Property data={this.state.data} style={styles.viewborder} />
-                                <BuyNum style={styles.viewborder} />
+                                <BuyNum data={this.state.data}  changeBuyNum={(num) => {
+                                    this.changeBuyNum(num)
+                                }}
+                                    style={styles.viewborder}
+                                />
                             </View>
                             {
                                 this.state.data.description.length > 0 || this.state.data.imgdes.length > 0 || this.state.data.detaildes.length > 0 || this.state.data.featuredes.length > 0
@@ -91,23 +85,6 @@ export default class Product extends Component {
                         </ScrollView>
                         : <Loading />
                 }
-                {/* {
-                    this.state.data.count > 0
-                        ? 
-                        : null
-                } */}
-                {/* 
-                    <View style={styles.viewborder}>
-                        <Text style={{ fontSize: 13, color: '#adadad', marginLeft: 10 }}>商品介绍</Text>
-                        <AutoHeightWebView
-                            originWhitelist={['*']}
-                            source={{ html: '<div style="font-size:0.8rem"><p>  功能特点：设计新颖、外观精巧、刀锋锐安全 </p></div>' }}
-                        />
-                        <AutoHeightWebView
-                            originWhitelist={['*']}
-                            source={{ html: Tools.imgHTMLAddWidth('<div style="font-size:0.8rem><p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/25224/30/2405/306629/5c1c7abcE20119d2c/fb55f6107a23c920.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/24030/18/2483/132267/5c1c7abcE6d90720b/8ca7a2a054c43cdc.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/29095/19/2399/186720/5c1c7abcEef845f14/9e772372b41f6878.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/20851/33/2417/177570/5c1c7abcE5ce1ee3f/2d0a311c05e4c055.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/28725/18/2371/186295/5c1c7abcEf44330f7/7c77f10e245e43ba.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/26621/9/2438/200861/5c1c7abcEf980b06a/2887575669250aaf.jpg" align="absmiddle" /> </p> <p>  <img class="" src="https://img10.360buyimg.com/imgzone/jfs/t1/14789/33/2419/263276/5c1c7abcE090de2f1/3e29ca4fd7171697.jpg" align="absmiddle" /> </p></div>') }}
-                        />
-                    </View> */}
                 {
                     this.state.data.count > 0
                         ? <View style={{ flexDirection: 'row', borderTopColor: '#f3f3f3', borderTopWidth: 1 }}>
@@ -118,6 +95,7 @@ export default class Product extends Component {
                             >
                                 <ShoppingCart />
                             </TouchableOpacity>
+
                             <TouchableOpacity
                                 style={{ width: '35%', height: 50 }}
                                 onPress={() => {
@@ -125,9 +103,11 @@ export default class Product extends Component {
                             >
                                 <Text style={[styles.button, { backgroundColor: '#fe9402', color: '#fff' }]}>加入购物车</Text>
                             </TouchableOpacity>
+
                             <TouchableOpacity
                                 style={{ width: '35%', height: 50 }}
                                 onPress={() => {
+                                    alert(this.state.buyNum)
                                 }}
                             >
                                 <Text style={[styles.button, { backgroundColor: 'red', color: '#fff' }]}>立即购买</Text>
@@ -141,68 +121,6 @@ export default class Product extends Component {
     }
 }
 
-//购买处理弹出框
-class BuyNumModal extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            modalVisible: false,
-            buyNum: props.buyNum
-        }
-    }
-
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.showModal) {
-            this.setModalVisible(true)
-        }
-    }
-    setModalVisible = (visible) => {
-        // if (!visible) {
-        //     this.props.changeBuyNum(this.state.buyNum)
-        // }
-        this.setState({ modalVisible: visible });
-
-    }
-    render() {
-        return (
-            <Modal
-                animationType='fade'
-                transparent={true}
-                visible={this.state.modalVisible}
-                onRequestClose={() => {
-
-                }}
-            >
-                <View style={{ flex: 1, flexDirection: 'column-reverse', backgroundColor: 'rgba(0, 0, 0, 0.3)' }}>
-                    <View style={{ backgroundColor: '#fff', height: 200 }}>
-                        <Text
-                            onPress={this.setModalVisible.bind(this, false)}
-                            style={{ fontSize: 20, marginTop: 10 }}>
-                            关闭
-                        </Text>
-                        <TextInput
-                            style={{ height: 30, width: 100, borderColor: 'red', borderWidth: 1, padding: 5 }}
-                            value={this.state.buyNum.toString()}
-                            onChangeText={(text) => {
-                                this.setState({
-                                    buyNum: text
-                                },
-                                    this.props.changeBuyNum(text)
-                                )
-                            }}
-                        />
-                    </View>
-
-                    <TouchableOpacity
-                        onPress={this.setModalVisible.bind(this, false)}
-                    >
-                        <View style={{ height: 300 }}></View>
-                    </TouchableOpacity>
-                </View>
-            </Modal>
-        )
-    }
-}
 // 左下方购物车
 class ShoppingCart extends Component {
     render() {
